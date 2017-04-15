329e2a879d4eb689a548138fd0a26d645e1009d4
Jakewilliams22 <Jakemwilliams2@gmail.com>
2017-04-15 01:00:39 -0400

    Tensor Attempt?

--- test.py
+++ test.py
@@ -61,9 +61,6 @@ def numDates(file):
 
     return return_row
 
-def dateParser
-
-
 
 def getFeatureResultFormat(productTriples):
     features = []; #[two days ago views][one day ago views]
@@ -74,7 +71,7 @@ def getFeatureResultFormat(productTriples):
             twoAgo = triple[0][1]        
             oneAgo = triple[1][1];        
             features.append([twoAgo, oneAgo]);
-            classifications.append([triple[2][1]]);
+            classifications.append(triple[2][1]);
 
     return features, classifications;
 
@@ -91,16 +88,46 @@ def splitTestTrain(features, classes, breakPercent):
 trips = loadData('./TimeSeriesPredictionTrain.csv');
 features, classes = getFeatureResultFormat(trips)
 train, test = splitTestTrain(features, classes, .8);
-xTr, yTr = train;
-xTe, yTe = test;
+Xtr, Ytr = train;
+Xte, Yte = test;
         
     
-    
+# tf Graph Input
+xtr = tf.placeholder("float", [None, 784])
+xte = tf.placeholder("float", [784])
+
+# Nearest Neighbor calculation using L1 Distance
+# Calculate L1 Distance
+distance = tf.reduce_sum(tf.abs(tf.add(xtr, tf.negative(xte))), reduction_indices=1)
+# Prediction: Get min distance index (Nearest neighbor)
+pred = tf.arg_min(distance, 0)
+
+accuracy = 0.
+
+# Initializing the variables
+init = tf.global_variables_initializer()
+
+# Launch the graph
+with tf.Session() as sess:
+    sess.run(init)
+
+    # loop over test data
+    for i in range(len(Xte)):
+        # Get nearest neighbor
+        nn_index = sess.run(pred, feed_dict={xtr: Xtr, xte: Xte[i, :]})
+        # Get nearest neighbor class label and compare it to its true label
+        ##print("Test", i, "Prediction:", np.argmax(Ytr[nn_index]), \
+        ##    "True Class:", np.argmax(Yte[i]))
+        # Calculate accuracy
+        if np.argmax(Ytr[nn_index]) == np.argmax(Yte[i]):
+            accuracy += 1./len(Xte)
+    print("Done!")
+    print("Accuracy:", accuracy)
 
 
 
 
 
 
-print( numDates( './TimeSeriesPredictionTrain.csv' ) );
+#print( numDates( './TimeSeriesPredictionTrain.csv' ) );
 #print( printRow('./TimeSeriesPredictionTrain.csv', 0 ) );
