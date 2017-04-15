e37f6437ee9af98cd4667dd7cbd66e74796d36be
Graham Ely <firstclass140@gmail.com>
2017-04-15 05:33:37 -0400

    Minor code cleanup

--- main.py
+++ main.py
@@ -17,14 +17,15 @@ def getFeatureResultFormat(productTriples):
             oneAgo   = float(triple[3][1])
             features.append([fourAgo, threeAgo, twoAgo, oneAgo]);
             classifications.append(float(triple[4][1]));
-#        if(len(triple) == 3):        
-#            twoAgo = float(triple[0][1])
-#            oneAgo = float(triple[1][1]);        
-#            features.append([twoAgo, oneAgo]);
-#            classifications.append(float(triple[2][1]));
-#            arr = [0 for x in range(1,15000)];
-#            arr[int(triple[2][1])] = 1;
-#            classifications.append(arr);
+        # for varying k-value
+        if(len(triple) == 3):        
+            twoAgo = float(triple[0][1])
+            oneAgo = float(triple[1][1]);        
+            features.append([twoAgo, oneAgo]);
+            classifications.append(float(triple[2][1]));
+            arr = [0 for x in range(1,15000)];
+            arr[int(triple[2][1])] = 1;
+            classifications.append(arr);
 
     return features, classifications;
 
@@ -35,10 +36,10 @@ def splitTestTrain(features, classes, breakPercent):
     
     randIndexes = random.sample(range(0, len(features)), len(features))
     
-    Xtr=[];
-    Xte=[]
-    Ytr=[]
-    Yte = [];
+    Xtr = []
+    Xte = []
+    Ytr = []
+    Yte = []
 
     for index in randIndexes:
         if(len(Xtr) <= int(len(features) * .9)):
@@ -56,16 +57,10 @@ train, test = splitTestTrain(features, classes, .8);
 Xtr, Ytr = train;
 Xte, Yte = test;
 
-print(Xte);
-
 Xtr = np.asarray(Xtr);
-print(Xtr);
 Xte = np.asarray(Xte);    
-print(Xte);
 Ytr = np.asarray(Ytr);
-print(Ytr);
 Yte = np.asarray(Yte);
-print(Yte);
 
 print(Xte);
 
@@ -109,6 +104,12 @@ with tf.Session() as sess:
         if ( Ytr[nn_index] <= (Yte[i] * 1.15) and Ytr[nn_index] >= (Yte[i] * .85) ):
             accuracy_percentage += 1./len(Xte)
         mav += abs( Ytr[nn_index] - (Yte[i]) )
+
+    print("Done!")
+    print("Accuracy (views within +-5):", accuracy)
+    print("Accuracy (views within 15%):", accuracy_percentage)
+    print("MAE:", (mav / len( Xte ) ) );
+
 #def runWithOneNeighbor():
 #    trips = loadData('./TimeSeriesPredictionTrain.csv');
 #    features, classes = getFeatureResultFormat(trips)
@@ -165,94 +166,60 @@ with tf.Session() as sess:
 #
 #
 ##runWithOneNeighbor();
-#
-#def runWithKNeighbors(K):
-#    trips = loadData('./TimeSeriesPredictionTrain.csv');
-#    features, classes = getFeatureResultFormat(trips)
-#    train, test = splitTestTrain(features, classes, .8);
-#    Xtr, Ytr = train;
-#    Xte, Yte = test;
-#
-#
-#    Xtr = np.asarray(Xtr);
-#    print(Xtr);
-#    Xte = np.asarray(Xte);    
-#    print(Xte);
-#    Ytr = np.asarray(Ytr);
-#    print(Ytr);
-#    Yte = np.asarray(Yte);
-#    print(Yte);
-#
-#    # tf Graph Input
-#    xtr = tf.placeholder("float", [None, 2])
-#    ytr = tf.placeholder("float", Ytr.shape)
-#    xte = tf.placeholder("float", [2])
-#
-#
-#    # Euclidean Distance
-#    distance = tf.negative(tf.sqrt(tf.reduce_sum(tf.square(tf.subtract(xtr, xte)), reduction_indices=1)))
-#    # Prediction: Get min distance neighbors
-#    values, indices = tf.nn.top_k(distance, k=K, sorted=False)
-#
-#    nearest_neighbors = []
-#    for i in range(K):
-#        nearest_neighbors.append(tf.argmax(ytr[indices[i]], 0))
-#
-#    neighbors_tensor = tf.stack(nearest_neighbors)
-#    y, idx, count = tf.unique_with_counts(neighbors_tensor)
-#    pred = tf.slice(y, begin=[tf.argmax(count, 0)], size=tf.constant([1], dtype=tf.int64))[0]
-#
-#    accuracy = 0.
-#
-#    # Initializing the variables
-#    init = tf.initialize_all_variables()
-#
-#    # Launch the graph
-#    with tf.Session() as sess:
-#        sess.run(init)
-#
-#        # loop over test data
-#        for i in range(len(Xte)):
-#            # Get nearest neighbor
-#            nn_index = sess.run(pred, feed_dict={xtr: Xtr, ytr: Ytr, xte: Xte[i, :]})
-#            # Get nearest neighbor class label and compare it to its true label
-#            print("Test", i, "Prediction:", nn_index,
-#                 "True Class:", np.argmax(Yte[i]))
-#            #Calculate accuracy
-#            if nn_index == np.argmax(Yte[i]):
-#                accuracy += 1. / len(Xte)
-#        print("Done!")
-#        print("Accuracy:", accuracy)
-#
-#
-#
-#runWithKNeighbors(4);
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
 
-    print("Done!")
-    print("Accuracy (views within +-5):", accuracy)
-    print("Accuracy (views within 15%):", accuracy_percentage)
-    print("MAE:", (mav / len( Xte ) ) );
-    print( numDates( './TimeSeriesPredictionTrain.csv' ) );
+def runWithKNeighbors(K):
+    trips = loadData('./TimeSeriesPredictionTrain.csv');
+    features, classes = getFeatureResultFormat(trips)
+    train, test = splitTestTrain(features, classes, .8);
+    Xtr, Ytr = train;
+    Xte, Yte = test;
+
+
+    Xtr = np.asarray(Xtr);
+    print(Xtr);
+    Xte = np.asarray(Xte);    
+    print(Xte);
+    Ytr = np.asarray(Ytr);
+    print(Ytr);
+    Yte = np.asarray(Yte);
+    print(Yte);
+
+    # tf Graph Input
+    xtr = tf.placeholder("float", [None, 2])
+    ytr = tf.placeholder("float", Ytr.shape)
+    xte = tf.placeholder("float", [2])
+
+
+    # Euclidean Distance
+    distance = tf.negative(tf.sqrt(tf.reduce_sum(tf.square(tf.subtract(xtr, xte)), reduction_indices=1)))
+    # Prediction: Get min distance neighbors
+    values, indices = tf.nn.top_k(distance, k=K, sorted=False)
+
+    nearest_neighbors = []
+    for i in range(K):
+        nearest_neighbors.append(tf.argmax(ytr[indices[i]], 0))
+
+    neighbors_tensor = tf.stack(nearest_neighbors)
+    y, idx, count = tf.unique_with_counts(neighbors_tensor)
+    pred = tf.slice(y, begin=[tf.argmax(count, 0)], size=tf.constant([1], dtype=tf.int64))[0]
+
+    accuracy = 0.
+
+    # Initializing the variables
+    init = tf.initialize_all_variables() #
+    # Launch the graph
+    with tf.Session() as sess:
+        sess.run(init)
+
+        # loop over test data
+        for i in range(len(Xte)):
+            # Get nearest neighbor
+            nn_index = sess.run(pred, feed_dict={xtr: Xtr, ytr: Ytr, xte: Xte[i, :]})
+            # Get nearest neighbor class label and compare it to its true label
+            print("Test", i, "Prediction:", nn_index,
+                 "True Class:", np.argmax(Yte[i]))
+            #Calculate accuracy
+            if nn_index == np.argmax(Yte[i]):
+                accuracy += 1. / len(Xte)
+        print("Done!")
+        print("Accuracy:", accuracy)
